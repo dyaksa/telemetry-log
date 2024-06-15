4e070bcabaaac4db564d58f991bfe5e8f19314f7
dyaksa <diasnour0395@gmail.com>
2024-06-16 00:15:06 +0700

    docs: err trace

--- err/errtrace.go
+++ err/errtrace.go
@@ -1,3 +1,4 @@
+// Package err provides functionality for error tracing.
 package err
 
 import (
@@ -6,20 +7,26 @@ import (
 	"strconv"
 )
 
+// errStack is a struct that holds the necessary information for an error stack.
 type errStack struct {
-	Name string `bson:"name,omitempty" json:"name"`
-	File string `bson:"file,omitempty" json:"file"`
-	Line string `bson:"line,omitempty" json:"line"`
+	Name string `bson:"name,omitempty" json:"name"` // Name is the name of the function where the error occurred.
+	File string `bson:"file,omitempty" json:"file"` // File is the name of the file where the error occurred.
+	Line string `bson:"line,omitempty" json:"line"` // Line is the line number where the error occurred.
 }
 
+// ErrorTracer is a struct that holds the necessary information for error tracing.
 type ErrorTracer struct {
-	stackTrace []uintptr
+	stackTrace []uintptr // stackTrace is a slice of program counters.
 }
 
+// Error is a method that returns an empty string.
+// It is required to satisfy the error interface.
 func (errTrace *ErrorTracer) Error() (s string) {
 	return
 }
 
+// callers is a function that returns a slice of program counters.
+// It skips the first 5 callers.
 func callers() []uintptr {
 	const depth = 32
 	var pcs [depth]uintptr
@@ -27,6 +34,8 @@ func callers() []uintptr {
 	return pcs[0:n]
 }
 
+// Print is a method that returns a slice of errStack.
+// It creates an errStack for each program counter in the stack trace.
 func (e *ErrorTracer) Print() []errStack {
 	var traces []errStack
 
@@ -42,6 +51,7 @@ func (e *ErrorTracer) Print() []errStack {
 	return traces
 }
 
+// Err is a method that returns a new ErrorTracer with the current stack trace.
 func (e *ErrorTracer) Err() error {
 	return &ErrorTracer{
 		stackTrace: callers(),
