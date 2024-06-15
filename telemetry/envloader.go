1efcfd08efafb39a6909f7dcd2cdaf6c204bd14c
dyaksa <diasnour0395@gmail.com>
2024-06-16 00:15:59 +0700

    docs: env loader

--- telemetry/envloader.go
+++ telemetry/envloader.go
@@ -1,3 +1,4 @@
+// Package telemetry provides functionality for telemetry logging.
 package telemetry
 
 import (
@@ -6,11 +7,22 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// LoadEnv is a function that loads environment variables from a .env file into a given struct.
+// It uses the "env" tag in the struct to match the environment variables.
+// If the .env file cannot be loaded or the environment variables cannot be parsed, it returns an error.
 func LoadEnv(v interface{}) (err error) {
+	// Load environment variables from .env file
 	err = godotenv.Load()
+
+	// Define options for parsing environment variables
 	opts := env.Options{TagName: "env"}
+
+	// Parse environment variables into the given struct
 	if err = env.ParseWithOptions(v, opts); err != nil {
+		// If an error occurs, join it with a new error message
 		err = errors.Join(err, errors.New("fail to load env"))
 	}
+
+	// Return any error that occurred
 	return
 }
